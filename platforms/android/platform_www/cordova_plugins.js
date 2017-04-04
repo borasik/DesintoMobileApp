535fd63a3081c973216450a149984db9e375e7a5
Alex Borsuk <alex.borsuk@dh.com>
2017-04-04 17:15:13 -0400

    Initial commit

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "cordova-plugin-appavailability.AppAvailability",
+        "file": "plugins/cordova-plugin-appavailability/www/AppAvailability.js",
+        "pluginId": "cordova-plugin-appavailability",
+        "clobbers": [
+            "appAvailability"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.3.2",
+    "cordova-plugin-appavailability": "0.4.2"
+};
+// BOTTOM OF METADATA
+});
